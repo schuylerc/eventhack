91a8cd3d3c5a2fd546d21b90ebbdddfdf30e70fb
Schuyler Cumbie <schuylerc@gmail.com>
2014-03-01 17:36:47 -0500

    encryption key added

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '1k23h1k2jqegp7sfgao7yroSIDftaosgubwiE';
 
 /*
 |--------------------------------------------------------------------------
