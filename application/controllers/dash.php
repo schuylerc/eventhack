9019c26b2cef2be9f30d0b67af6768e5ab766259
Schuyler Cumbie <schuylerc@gmail.com>
2014-03-01 16:12:21 -0500

    added starter files

---
+++ application/controllers/dash.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Dash extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		//$this->checkLogin();
+	}
+	
+	public function index(){
+	
+		//$this->beginView();
+		//get vars
+		//$this->load->view('connect/home');
+		//$this->endView();
+	
+	}
+	public function test(){
+		//$this->load->view('folder/namewithoutphp');
+	}
+}
