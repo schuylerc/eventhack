84b6100e3c9845a569b3bd79d5c1f4c832466eb4
Schuyler Cumbie <schuylerc@gmail.com>
2014-03-01 17:22:47 -0500

    added migration functions. please make sure you give localhost access to an empty database for the user eventhack with db name eventhack

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'eventhack';
+$db['default']['password'] = 'winners';
+$db['default']['database'] = 'eventhack';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
---
+++ application/controllers/migrate.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migrate extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+	}
+	public function index(){
+		$this->load->library('migration');
+		if ( ! $this->migration->current()){
+			show_error($this->migration->error_string());
+		}
+	}
+}
