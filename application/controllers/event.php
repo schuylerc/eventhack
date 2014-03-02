83092cdd5a4ffa4bea246aa82ba2d7e7b9fe2135
Schuyler Cumbie <schuylerc@gmail.com>
2014-03-02 01:41:49 -0500

    Controllers now extend core custom controller

--- application/controllers/event.php
+++ application/controllers/event.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Event extends CI_Controller {
+class Event extends MY_Controller {
 	public function __construct()
 	{
 		parent::__construct();
