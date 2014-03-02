7d1c3aa2772ac9e548cd223313ec10701ab94041
Christopher Bednar <bednar.christopher@gmail.com>
2014-03-02 05:38:21 -0500

    fixed the broken link, linked it back to the dash

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -32,7 +32,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.html">eventHACK</a>
+          <a class="navbar-brand" href="/dash/">eventHACK</a>
         </div>
           <ul class="nav navbar-nav navbar-right navbar-user">
             
