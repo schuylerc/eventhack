0ebeef7fd2d4671d8462296735623dbc9cdd29df
Schuyler Cumbie <schuylerc@gmail.com>
2014-03-02 04:47:44 -0500

    added begin and end view functions to the test dash function

--- application/controllers/dash.php
+++ application/controllers/dash.php
@@ -21,7 +21,9 @@ class Dash extends MY_Controller {
 	}
 	
 	public function test(){
+		$this->beginView();
 		$this->load->view('event/view');
+		$this->endView();
 	}
 	
 	public function loadMap(){
