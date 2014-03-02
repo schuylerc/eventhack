de98b0e56a09a0f3d410852e54933f6450849621
Christopher Bednar <bednar.christopher@gmail.com>
2014-03-02 04:44:10 -0500

    removed <html><head><title>

--- application/views/event/view.php
+++ application/views/event/view.php
@@ -1,29 +1,4 @@
-<html>
 
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="">
-<meta name="author" content="">
-
-<title>Dashboard - SB Admin</title>
-
-<!-- Bootstrap core CSS -->
-<link href="/assets/css/bootstrap.css" rel="stylesheet">
-
-<!-- Add custom CSS here -->
-<link href="/assets/css/sb-admin.css" rel="stylesheet">
-<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
-<!-- Page Specific CSS -->
-<link rel="stylesheet"
-	href="http://cdn.oesmith.co.uk/morris-0.4.3.min.css">
-<link rel="stylesheet" 
-	href="/assets/css/event.css">
-	
-<!-- countdown -->
-<script src="/assets/js/countdown.js"></script>
-
-</head>
 <body>
 
 	<div class="row-fluid">
@@ -63,17 +38,17 @@
 		
 	</div> <!-- includes parking info, campus map(google map api), floor plans -->
 	
-	<div class = "row-fuild">
+	<div class = "row-fluid">
 		<div class = "col-lg-12 event-when">
 			<h2 class = "event-heading">When:</h2>
 			<p class = "event-date-text"><?php echo date("l, F jS, Y",strtotime($eDetail->start_time)); ?></p>
 			<p class = "event-date-text">at <?php echo date("h:iA",strtotime($eDetail->start_time)); ?></p>
-			
-			<script>
-				var myCountdown1 = new Countdown({time:316});
-			</script>
+			<div align = "center" class = "col-lg-12">		
+				<script>
+					var myCountdown1 = new Countdown({time:316});
+				</script>
+			</div>
 		</div>
-		
 	</div>
 	
 	<div class = "row-fluid">
@@ -84,4 +59,3 @@
 	</div> <!--  some secondary information -->
 	
 </body>
-</html>
